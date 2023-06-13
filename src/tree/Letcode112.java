ed1446a16208535f1cc899fb6a5f22401d1e9dbb
罗晨 <1641767724@qq.com>
2023-06-13 19:17:34 +0800

    6/13 树的总路径

---
+++ src/tree/Letcode112.java
@@ -0,0 +1,46 @@
+package tree;
+
+
+/**
+ * @Author Lc
+ * @Date 2023/6/13
+ * @Description
+ */
+public class Letcode112 {
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(5);
+        TreeNode LeftTreeNode4 = new TreeNode(4);
+        TreeNode LeftTreeNode11 = new TreeNode(11);
+        TreeNode LeftTreeNode7 = new TreeNode(7);
+        TreeNode RightTreeNode2 = new TreeNode(2);
+        treeNode.left = LeftTreeNode4;
+        LeftTreeNode4.left = LeftTreeNode11;
+        LeftTreeNode11.left = LeftTreeNode7;
+        LeftTreeNode11.right = RightTreeNode2;
+        boolean b = hasPathSum(treeNode, 22);
+    }
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+        //回溯
+        targetSum -= root.val;
+        //到遍历到根节点了 判断是为0
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
+
+        if(root.left != null){
+            if(hasPathSum(root.left,targetSum)){
+                return true;
+            }
+        }
+
+        if(root.right != null){
+            if(hasPathSum(root.right,targetSum)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/tree/TreeNode.java
@@ -0,0 +1,19 @@
+package tree;
+
+/**
+ * @Author Lc
+ * @Date 2023/6/13
+ * @Description
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
