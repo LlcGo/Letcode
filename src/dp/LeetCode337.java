42c40697f0fb9b562f0756a57b5536007151fc0b
罗晨 <1641767724@qq.com>
2023-09-05 16:58:30 +0800

    9/4 打家劫舍Ⅲ

---
+++ src/dp/LeetCode337.java
@@ -0,0 +1,36 @@
+package dp;
+
+import tree.TreeNode;
+
+/**
+ * @Author Lc
+ * @Date 2023/9/5
+ * @PackageName: dp
+ * @ClassName: LeetCode337
+ * @Description: 大家劫舍Ⅲ
+ */
+
+public class LeetCode337 {
+    // 不偷：Max(左孩子不偷，左孩子偷) + Max(又孩子不偷，右孩子偷)
+    // root[0] = Math.max(rob(root.left)[0], rob(root.left)[1]) +
+    // Math.max(rob(root.right)[0], rob(root.right)[1])
+    // 偷：左孩子不偷+ 右孩子不偷 + 当前节点偷
+    // root[1] = rob(root.left)[0] + rob(root.right)[0] + root.val;
+    public int rob(TreeNode root) {
+        int[] res = robAction1(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    int[] robAction1(TreeNode root) {
+        int res[] = new int[2];
+        if (root == null)
+            return res;
+
+        int[] left = robAction1(root.left);
+        int[] right = robAction1(root.right);
+
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] + right[0];
+        return res;
+    }
+}
