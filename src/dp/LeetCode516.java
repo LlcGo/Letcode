1160b0a40de3309df47817ae393a5bdf470defa3
罗晨 <1641767724@qq.com>
2023-09-30 16:44:17 +0800

    516.最长回文子串序列

---
+++ src/dp/LeetCode516.java
@@ -0,0 +1,27 @@
+package dp;
+
+/**
+ * @Author Lc
+ * @Date 2023/9/30
+ * @PackageName: dp
+ * @ClassName: LeetCode516
+ * @Description: 最长回文子序列
+ */
+
+public class LeetCode516 {
+    public int longestPalindromeSubseq(String s) {
+        int len = s.length();
+        int[][] dp = new int[len + 1][len + 1];
+        for (int i = len - 1; i >= 0; i--) { // 从后往前遍历 保证情况不漏
+            dp[i][i] = 1; // 初始化
+            for (int j = i + 1; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], Math.max(dp[i][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[0][len - 1];
+    }
+}
